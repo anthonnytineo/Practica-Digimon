b98d0462dd5c34b8a7c6db8c93494fd6feb4f68e
anthonny tineo <anthonny@MacBook-Pro-de-anthonny.local>
2021-05-17 21:01:52 -0400

    Creacion de app cartas digimon

---
+++ src/Digimon.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+
+const Digimon = (props) => {
+
+    return (
+        <div className="digiCard">
+            <div className="nombre">{props.nombre}</div>
+            <div className="nivel">Nivel: {props.nivel}</div>
+            <img className="imagen" src={props.img}/>
+        </div>
+    )
+}
+
+export default Digimon;
